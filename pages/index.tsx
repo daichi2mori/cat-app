ef1a37f9901aa5eef9b10ffaf83f01ba32eec5cc
daichi2mori <daichi2mori@gmail.com>
2022-11-14 11:32:45 +0900

    🚚 猫画像生成機能をindexに移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,57 @@
 import { GetServerSideProps, NextPage } from 'next'
+import { CircularProgress } from '@mui/material';
 import Link from 'next/link';
 import { useState } from 'react';
 
+interface SearchCatImage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
 
-const Home: NextPage = () => {
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await res.json();
+  // console.log(result[0]);
+  return result[0];
+}
+
+
+const Home: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+  const [spinner, setSpinner] = useState(false);
+
+  const handleClick = async () => {
+    setSpinner(true);
+    const catImage = await fetchCatImage();
+    setCatImageUrl(catImage.url);
+    setTimeout(() => {
+      setSpinner(false)
+    }, 1000);
+  }
+
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <Link href="/neko">
-        <button>
-          猫画像生成はこちら
-        </button>
-      </Link>
+      <h1>猫画像アプリ</h1>
+      {spinner ? <CircularProgress /> : <img src={catImageUrl} alt="" width={400} height="auto" />}
+      <button onClick={handleClick} style={{ marginTop: 18 }}>今日の猫さん</button>
     </div>
   );
+};
+
+// SSR(サーバーサイドレンダリング)
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
+  const catImage = await fetchCatImage();
+  return {
+    props: {
+      initialCatImageUrl: catImage.url,
+    },
+  };
 };
 
 export default Home;
